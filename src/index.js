a29aa4a1d9095811e56f45cac15c1190e6f8bf78
Yuriy Lipchey <xaosmmm@gmail.com>
2021-03-09 08:39:50 +0200

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min =
+function min(array) {
+    if (!array || array.length === 0) return 0
+    let out = array[0]
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < out) out = array[i]
+    }
+    return out
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max =
+function max(array) {
+    if (!array || array.length === 0) return 0
+    let out = array[0]
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > out) out = array[i]
+    }
+    return out
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg =
+function avg(array) {
+    if (!array || array.length === 0) return 0
+    let out = 0
+    for (let i = 0; i < array.length; i++) {
+        out += array[i]
+    }
+    return out / array.length
 }
